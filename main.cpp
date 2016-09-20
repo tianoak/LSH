c00cd391f64b10c8d0b27b3d1a21c918b09ca368
tianoak <tian_oak@outlook.com>
2016-09-20 21:23:46 +0800

    c++ lsh algorithm
    first commit, exist errors not solved yet.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "params.h"
+#include "flann.h"
+#include <map>
+#include <cstring>
+#include "any.h"
+
+#include <iostream>
+using namespace std;
+using namespace flann;
+
+
+
+int main()
+{
+	flann_distance_t flann_distance_type = FLANN_DIST_EUCLIDEAN;
+	int flann_distance_order = 3;
+
+	return 0;
+}
